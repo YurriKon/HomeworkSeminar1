15b848aecb72e605799114a887fee89a6fa66f71
yurrikon <yurrikon@gmail.com>
2022-06-12 00:58:57 +0300

    Task 2 completed

---
+++ Task2.cs
@@ -0,0 +1,19 @@
+Console.WriteLine("Семинар 1 ДЗ Задача 2");
+Console.WriteLine("Введите первое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    Console.WriteLine("max " + a);
+    Console.WriteLine("min " + b);
+}
+if (b > a)
+{
+    Console.WriteLine("max " + b);
+    Console.WriteLine("min " + a);
+}
+else
+{
+    Console.WriteLine("Числа равны");
+}
